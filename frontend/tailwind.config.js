31c5bfe2acb7e33d2e8c28376df66cc1bfb07996
Abimanyuoke123 <fananiabimanyu@gmail.com>
2025-07-30 06:48:54 +0700

    `Removed antialiased class from body element in RootLayout and updated tailwind config to reflect the change`

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-<<<<<<< HEAD
     "./app/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
@@ -49,18 +48,3 @@ module.exports = {
   },
   plugins: [],
 };
-=======
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
->>>>>>> f6ac4fc754a9bbdb07d403624728bdd6bc807ee0
